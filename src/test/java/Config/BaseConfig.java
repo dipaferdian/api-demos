51649849c1d320de5cece223b146ffc1ba93921c
dipaferdian <dipaferdian2@gmail.com>
2023-08-21 13:44:28 +0700

    fix test method

---
+++ src/test/java/Config/BaseConfig.java
@@ -0,0 +1,95 @@
+package Config;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+public class BaseConfig {
+
+    protected AndroidDriver<MobileElement> driver;
+    protected WebDriverWait wait;
+
+    @Before
+    public void setUp() {
+
+        Properties properties = new Properties();
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+
+            String PLATFORM_NAME    = properties.getProperty("PLATFORM_NAME");
+            String PLATFORM_VERSION = properties.getProperty("PLATFORM_VERSION");
+            String DEVICE_NAME      = properties.getProperty("DEVICE_NAME");
+            String APP              = properties.getProperty("APP");
+            String APP_PACKAGE      = properties.getProperty("APP_PACKAGE");
+            String APP_ACTIVITY     = properties.getProperty("APP_ACTIVITY");
+            String APPIUM_HOST      = properties.getProperty("APPIUM_HOST");
+            String APPIUM_PORT      = properties.getProperty("APPIUM_PORT");
+
+//            System.out.println(PLATFORM_NAME + PLATFORM_VERSION + DEVICE_NAME + APP + APP_PACKAGE + APP_ACTIVITY + APPIUM_HOST + APPIUM_PORT);
+
+            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+            caps.setCapability(MobileCapabilityType.PLATFORM_NAME,PLATFORM_NAME);
+            caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,PLATFORM_VERSION);
+            caps.setCapability(MobileCapabilityType.DEVICE_NAME,DEVICE_NAME);
+            caps.setCapability("app", APP);
+            caps.setCapability("appPackage",APP_PACKAGE);
+            caps.setCapability("appActivity",APP_ACTIVITY);
+
+            URL url = new URL(APPIUM_HOST + ":" + APPIUM_PORT);
+            driver = this.getAndroidDriver(url, caps);
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Before
+    public void showHomePage() {
+
+        MobileElement button_permission = driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"Continue\")"));
+        button_permission.click();
+
+        this.popupUpdate();
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("Close session");
+        driver.quit();
+    }
+
+    private void popupUpdate() {
+
+        WebDriverWait wait_until = wait(this.getDriver()); // 10 seconds timeout
+        WebElement popup_element = wait_until.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/button1")));
+
+        popup_element.click();
+    }
+
+    private WebDriverWait wait(AndroidDriver<MobileElement> driver) {
+        return this.wait = new WebDriverWait(driver, 10);
+    }
+
+    private AndroidDriver<MobileElement> getDriver(){
+        return driver;
+    }
+
+    private AndroidDriver<MobileElement> getAndroidDriver(URL url, DesiredCapabilities caps){
+        return new AndroidDriver<>(url,caps);
+    }
+}
--- src/test/java/VerifyApp.java
+++ src/test/java/VerifyApp.java
@@ -1,15 +1,54 @@
+import Config.BaseConfig;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class VerifyApp extends BaseConfig{
+
+public class VerifyApp extends BaseConfig {
 
     @Test
-    public void showAppPage() {
-         driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"App\")")).click();
+    public void step1_verifyAppPage() {
+        // Assert that the element is not null (exists)
+        assertNotNull(this.verifyAppElement(), "The element was not found on the screen");
+
+        // You can also perform further assertions on the element's properties or behavior
+        assertTrue(this.verifyAppElement().isDisplayed(), "The element is not displayed");
+
+        // Assert that the element's text is not empty
+        assertFalse(this.verifyAppElement().getText().isEmpty(), "The element's text is empty");
+
+        // Assert that the actual text matches the expected text
+        assertEquals("App",  this.verifyAppElement().getText(), "Element text did not match the expected value");
+
+        this.verifyAppElement().click();
+    }
+
+    @Test
+    public void step2_verifyActionBarPage() {
+
+        this.verifyAppElement().click();
+
+        // Assert that the element is not null (exists)
+        assertNotNull(this.verifyActionBarElement(), "The element was not found on the screen");
+
+        // You can also perform further assertions on the element's properties or behavior
+        assertTrue(this.verifyActionBarElement().isDisplayed(), "The element is not displayed");
+
+        // Assert that the element's text is not empty
+        assertFalse(this.verifyActionBarElement().getText().isEmpty(), "The element's text is empty");
+
+        // Assert that the actual text matches the expected text
+        assertEquals("Action Bar",  this.verifyActionBarElement().getText(), "Element text did not match the expected value");
+
+        this.verifyActionBarElement().click();
+    }
+
+    private MobileElement verifyAppElement (){
+        return driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"App\")"));
+    }
+
+    private MobileElement verifyActionBarElement (){
+        return driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"Action Bar\")"));
     }
 }
